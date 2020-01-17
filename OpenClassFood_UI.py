923947a9b41957f647adff4620c6d080c4b19064
AntoineMaurin <antoine.maurin26@gmail.com>
2020-01-17 16:07:55 +0100

    Documentation plus complète, dénut du codage de l'interface utilisateur, installation des packages flake8, requests et mysql-connector, et màj du readme

---
+++ OpenClassFood_UI.py
@@ -0,0 +1,44 @@
+"""Database connexion"""
+
+"""User Interface that using database data"""
+
+"""Display options"""
+
+print("\nBonjour ! Bienvenue sur OpenFoodRooms !\n")
+print("Choisissez une option en entrant le nombre associé à celle-ci\n")
+
+"""Choice of one item"""
+
+print(" 1. Quel aliment souhaitez-vous remplacer ?\n",
+      "2. Retrouver mes aliments substitués\n")
+
+entry = input("Quelle option choisissez-vous ? ")
+while True:
+    try:
+        entry = int(entry)
+        assert entry > 0 and entry < 3
+        break
+    except ValueError:
+        print(" 1. Quel aliment souhaitez-vous remplacer ?\n",
+              "2. Retrouver mes aliments substitués\n")
+
+        entry = input("Quelle option choisissez-vous ? ")
+    except AssertionError:
+        print(" 1. Quel aliment souhaitez-vous remplacer ?\n",
+              "2. Retrouver mes aliments substitués\n")
+
+        entry = input("Quelle option choisissez-vous ? ")
+print("Vous avez choisi l'option ", entry)
+
+"""Choice 1 : display food"""
+#if entry == 1:
+
+"""Ask database better product"""
+
+"""Storage product and substitute in 'favoris'"""
+
+"""Display result of actual research"""
+
+"""Choice 2 : display ancient researches and result"""
+#elif entry == 2:
+"""Ask database to show ancient researches"""
---
+++ database_and_API.py
@@ -0,0 +1,5 @@
+"""Database creation : tables and fields"""
+
+"""API request on food categories"""
+
+"""Database filling with API data"""
