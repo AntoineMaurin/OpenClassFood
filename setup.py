c0fc433a4e903135f6931a8b84aab327d7601768
AntoineMaurin <antoine.maurin26@gmail.com>
2020-02-11 12:18:58 +0100

    README complété

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from database.database_setup import DatabaseSetup
 from API.data_from_api import DataFromApi
 
+# Creation of instances of the two imported classes
 setup = DatabaseSetup()
 data = DataFromApi()
 
+# Connexion to database with requested parameters
 setup.connect('localhost', 'student', 'password')
 
+# Creation of the database with the name in parameter
 setup.create_database('purbeurre')
 
+# Data receiving from API and adding in database
 data.request()
